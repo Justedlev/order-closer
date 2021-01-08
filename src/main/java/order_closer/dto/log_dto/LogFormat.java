6dc2f2c3b35742eca5494b3db0b2eb260d03a05b
justedlev <edyardlyk@gmail.com>
2021-01-08 17:38:20 +0200

    modified log format

--- src/main/java/order_closer/dto/log_dto/LogFormat.java
+++ src/main/java/order_closer/dto/log_dto/LogFormat.java
@@ -17,11 +17,11 @@ public class LogFormat {
     private String message;
 
     /**
-     * return log string in format "yyyy-MM-dd HH:mm:ss | TYPE_LOG [SERVICE_NAME] - MESSAGE"
+     * return log string in format "yyyy-MM-dd|HH:mm:ss TYPE_LOG [SERVICE_NAME] : MESSAGE"
      */
     @Override
     public String toString() {
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return String.format("%s | %s [%s] - %s", dateTime.format(format), messageType, service.getSimpleName(), message);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss");
+        return String.format("%s %s [%s] : %s", dateTime.format(format), messageType, service.getSimpleName(), message);
     }
 }
